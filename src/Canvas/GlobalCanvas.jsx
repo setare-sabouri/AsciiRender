727e1ccc3633827a83a3a13810a6f7c936736109
setareh <setarehsaboory@gmail.com>
2025-09-22 22:56:55 +0200

    first final

--- src/Canvas/GlobalCanvas.jsx
+++ src/Canvas/GlobalCanvas.jsx
@@ -4,28 +4,27 @@ import styles from './CanvasGlobal.module.scss'
 import Experience from "../Components/Experience";
 import { AsciiRenderer, Environment } from "@react-three/drei";
 import { useStore } from "../stores/useStore";
-
+import { useEffect, useRef } from "react";
 
 
 export default function Canvas3D() {
   const { ShowAscii,Inverted } = useStore()
 
+
   return (
     <div className={styles.canvasContainer} >
       <Canvas camera={{ position: [0, 1, 5], fov: 45 }} >
-        <Perf />
+        {/* <Perf /> */}
         <Environment preset="studio" />
         <ambientLight intensity={10} />
         <Experience />
-       
+
         {ShowAscii && (
           <AsciiRenderer
             fgColor="rgb(0, 255, 0);"
             bgColor="transparent"
             resolution={0.3}
             invert={Inverted}
-            
-            
           />
         )}
 
